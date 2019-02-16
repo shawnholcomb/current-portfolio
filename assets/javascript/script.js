db05708064e983b9f4b58eb6421ed605acd40003
Shawn Holcomb <sholcmb@gmail.com>
2019-02-16 00:06:58 -0600

    update arrows

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,13 +14,37 @@ $('.marvel-arrow').on('click', function () {
     $('#port-marvel').slideToggle('slow');
 });
 
-$('.down-arrow').on("click", function () {
-    $('.down-arrow').hide();
-    $('.up-arrow').show();
+$('#down-arrow-drinkr').on("click", function () {
+    $('#down-arrow-drinkr').hide();
+    $('#up-arrow-drinkr').show();
 });
-$('.up-arrow').on("click", function () {
-    $('.up-arrow').hide();
-    $('.down-arrow').show();
+$('#up-arrow-drinkr').on("click", function () {
+    $('#up-arrow-drinkr').hide();
+    $('#down-arrow-drinkr').show();
+})
+$('#down-arrow-tp').on("click", function () {
+    $('#down-arrow-tp').hide();
+    $('#up-arrow-tp').show();
+});
+$('#up-arrow-tp').on("click", function () {
+    $('#up-arrow-tp').hide();
+    $('#down-arrow-tp').show();
+})
+$('#down-arrow-nick').on("click", function () {
+    $('#down-arrow-nick').hide();
+    $('#up-arrow-nick').show();
+});
+$('#up-arrow-nick').on("click", function () {
+    $('#up-arrow-nick').hide();
+    $('#down-arrow-nick').show();
+})
+$('#down-arrow-marvel').on("click", function () {
+    $('#down-arrow-marvel').hide();
+    $('#up-arrow-marvel').show();
+});
+$('#up-arrow-marvel').on("click", function () {
+    $('#up-arrow-marvel').hide();
+    $('#down-arrow-marvel').show();
 })
 
 if ($(window).width() <= 1200) {
